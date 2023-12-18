3f55c9b70c10a223f08bbed726549dae7653f5a1
Hasan Moughnieh <108143194+Hasanmog@users.noreply.github.com>
2023-12-18 10:44:16 +0200

    Update sqlite_to_excel.py

--- sqlite_to_excel.py
+++ sqlite_to_excel.py
@@ -26,12 +26,10 @@ excel_path = f'{table_name}.xlsx'
 # Connect to the SQLite database
 conn = sqlite3.connect(db_path)
 
-# Assuming you want to export a table named 'my_table' from the SQLite database
-
 # Read the data from SQLite into a pandas DataFrame
 df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
 
-# Close the SQLite connection
+
 conn.close()
 
 # Write the data to an Excel file
